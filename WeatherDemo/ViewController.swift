1cad423678a1e2eb62834f97e63dd18c4650fe7d
An Xu <titianxu@gmail.com>
2019-08-07 16:39:05 +1000

    Initial Commit

---
+++ WeatherDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by An Xu on 7/8/19.
+//  Copyright © 2019 An Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
